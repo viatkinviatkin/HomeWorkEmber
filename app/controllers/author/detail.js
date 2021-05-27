ba2a6240b0aef73eb4bc66cbc2acc25efa611d72
Семён Вяткин <savyatkin@edu.hse.ru>
2021-05-27 15:22:56 +0500

    Дз 3

--- app/controllers/author/detail.js
+++ app/controllers/author/detail.js
@@ -5,7 +5,6 @@ export default Controller.extend({
   dataService: service('data'),
   actions: {
     async deleteAuthor(author) {
-
       try{
      await this.get('dataService').deleteAuthor(author);
      this.transitionToRoute('author')
